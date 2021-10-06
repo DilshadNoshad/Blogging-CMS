4979ed29b6b5f3fb78aa2aee1317099a8f6eb62b
DilshadSheikh <dilshadnoshad2003@gmail.com>
2021-10-06 19:32:03 +0500

    fetch post text data

--- index.php
+++ index.php
@@ -15,23 +15,33 @@
                     Page Heading
                     <small>Secondary Text</small>
                 </h1>
+<?php
+        $query = 'SELECT * FROM posts';
+        $select_all_posts = mysqli_query($connection, $query);
+
+        while($row = mysqli_fetch_assoc($select_all_posts)){
+                $post_title = $row['post_title'];
+                $post_author = $row['post_author'];
+                $post_date = $row['post_date'];
+                $post_content = $row['post_content'];
+                ?>
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#">Blog Post Title</a>
+                    <a href="#"><?php echo $post_title ?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
+                    by <a href="index.php"><?php echo $post_author ?></a>
                 </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
+                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
                 <hr>
                 <img class="img-responsive" src="http://placehold.it/900x300" alt="">
                 <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+                <p><?php echo $post_content ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
-
+                <?php  }  ?>
                 <!-- Pager -->
                 <ul class="pager">
                     <li class="previous">
