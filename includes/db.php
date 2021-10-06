c67e37298f4a02c61614006b39db89c07e257ffc
DilshadSheikh <dilshadnoshad2003@gmail.com>
2021-10-06 15:31:45 +0500

    connected to database
     update db.php

---
+++ includes/db.php
@@ -0,0 +1,20 @@
+<?php
+
+$db['db_host'] = 'localhost';
+$db['db_user'] = 'root';
+$db['db_pass'] = '';
+$db['db_name'] = 'blogging-cms';
+
+foreach($db as $key => $value){
+    define(strtoupper($key), $value);
+}
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if(!$connection){
+    die("Database broken not connected" . mysqli_error());
+}else{
+    echo "Database Connected Sucessfully";
+}
+
+?>
