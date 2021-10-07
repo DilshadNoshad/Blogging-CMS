3bc33ed1928f0298ac6626a011a36579ca85e6c7
DilshadSheikh <dilshadnoshad2003@gmail.com>
2021-10-07 18:13:32 +0500

    make widget  fetch data

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -17,23 +17,37 @@
 </div>
 
 <!-- Blog Categories Well -->
+<?php
+    $query = "SELECT * FROM categories";
+    $select_all_categories = mysqli_query($connection, $query);
+
+    if(!$select_all_categories){
+        die("query Failed" . mysqli_error($connection));
+    }
+?>
 <div class="well">
     <h4>Blog Categories</h4>
     <div class="row">
-        <div class="col-lg-6">
+        <div class="col-lg-12">
             <ul class="list-unstyled">
-                <li><a href="#">Category Name</a>
+                <?php
+                while($row = mysqli_fetch_assoc($select_all_categories)){
+                    $cat_title = $row['cat_title'];
+                    echo "<li><a href='#'>{$cat_title}</a></li>";
+                }
+                ?>
+                <!-- <li><a href="#">Category Name</a>
                 </li>
                 <li><a href="#">Category Name</a>
                 </li>
                 <li><a href="#">Category Name</a>
                 </li>
                 <li><a href="#">Category Name</a>
-                </li>
+                </li> -->
             </ul>
         </div>
         <!-- /.col-lg-6 -->
-        <div class="col-lg-6">
+        <!-- <div class="col-lg-6">
             <ul class="list-unstyled">
                 <li><a href="#">Category Name</a>
                 </li>
@@ -44,16 +58,13 @@
                 <li><a href="#">Category Name</a>
                 </li>
             </ul>
-        </div>
+        </div> -->
         <!-- /.col-lg-6 -->
     </div>
     <!-- /.row -->
 </div>
 
 <!-- Side Widget Well -->
-<div class="well">
-    <h4>Side Widget Well</h4>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-</div>
+<?php include 'widget.php' ?>
 
 </div>
