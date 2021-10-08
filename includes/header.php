dff80572d33c38db71f671c50e04456c982dec71
DilshadSheikh <dilshadnoshad2003@gmail.com>
2021-10-08 12:50:56 +0500

    admin page

--- includes/header.php
+++ includes/header.php
@@ -55,10 +55,10 @@
                 }
 
                 ?>
-                    <!-- <li>
-                        <a href="#">About</a>
-                    </li>
                     <li>
+                        <a href="admin">Admin</a>
+                    </li>
+                    <!-- <li>
                         <a href="#">Services</a>
                     </li>
                     <li>
