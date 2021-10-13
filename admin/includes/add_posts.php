05cc2e6aaf3cbcacd090c6709cd7c67ba49b400d
DilshadSheikh <dilshadnoshad2003@gmail.com>
2021-10-14 01:26:53 +0500

    update code in add post

--- admin/includes/add_posts.php
+++ admin/includes/add_posts.php
@@ -1,7 +1,24 @@
 <?php
+if(isset($_POST['create_post'])){
+$post_title =  $_POST['title'];
+$post_author =  $_POST['author'];
+$post_category_id =  $_POST['post_category_id'];
+$post_status =  $_POST['post_status'];
+
+$post_image = $_FILES['post_image']['name'];
+$post_image_temp = $_FILES['post_image']['tmp_name'];
+
+$post_tags =  $_POST['post_tags'];
+$post_content =  $_POST['post_content'];
+
+$post_date =  date('d-m-y');
+$post_comment_count =  4;
+
+    move_uploaded_file($post_image_temp, "../img/$post_image");
+}
 ?>
 
-<form action="" method="post" enctype="mutipart/form-data">
+<form action="" method="post" enctype="multipart/form-data">
 
     <div class="form-group">
         <label for="title">Post Title</label>
@@ -25,7 +42,7 @@
 
     <div class="form-group">
         <label for="post_image">Post Image</label>
-        <input type="file"name="post_image">
+        <input type="file" name="post_image">
     </div>
 
     <div class="form-group">
