73b913d2dbd1d2ba95652fdc4463f8e926d6a241
DilshadSheikh <dilshadnoshad2003@gmail.com>
2021-10-18 00:03:42 +0500

    display related category

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -31,7 +31,19 @@ echo "<tr>";
 echo "<td>{$post_id}</td>";
 echo "<td>{$post_author}</td>";
 echo "<td>{$post_title}</td>";
-echo "<td>{$post_category_id}</td>";
+
+$query = "SELECT * FROM categories WHERE cat_id = {$post_category_id}";
+$fetch_all_categories = mysqli_query($connection, $query);
+
+confirm_query($fetch_all_categories);
+
+while($row = mysqli_fetch_assoc($fetch_all_categories)){
+$cat_id = $row['cat_id'];
+$cat_title = $row['cat_title']; 
+
+echo "<td>$cat_title</td>";
+
+}
 echo "<td>{$post_status}</td>";
 echo "<td><img width='100' src='../img/{$post_image}' alt='image'></td>";
 echo "<td>{$post_tags}</td>";
